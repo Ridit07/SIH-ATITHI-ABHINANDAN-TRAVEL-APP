a057ea11671d642c81d8fc0a8bd4119bdf560639
Ridit07 <112847735+Ridit07@users.noreply.github.com>
2023-09-14 23:48:17 +0530

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ Budget = st.slider("Budget",10000,1000000)
 
 
 def predict():
-    float_features = [float(x) for x in [Traveller_Profile_Type, Choice_Preference, Mode_of_Travel, Budget]]
+    float_features = [float(x) for x in [Traveller Profile Type, Choice/Preference, Mode of Travel, Budget]]
     final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
     label = prediction[0]
