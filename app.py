5a4ff7fecbc40786a51622dbaa14d6b2e61be818
Ridit07 <112847735+Ridit07@users.noreply.github.com>
2023-09-14 23:21:19 +0530

    app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ model = pickle.load(open('decision_tree_model.pkl', 'rb'))
 st.title('which distination will he choose')
 
 Traveller_Profile_Type = st.slider("Traveller Profile Type",0,3)
-Choice/Preference = st.slider("Choice/Preference",0,3)
+Choice_Preference = st.slider("Choice/Preference",0,3)
 Mode_of_Travel = st.slider("Mode of Travel",0,3)
 Budget = st.slider("Budget",10000,1000000)
 
 
 
 def predict():
-    float_features = [float(x) for x in [Traveller_Profile_Type, Choice/Preference, Mode_of_Travel, Budget]]
+    float_features = [float(x) for x in [Traveller_Profile_Type, Choice_Preference, Mode_of_Travel, Budget]]
     final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
     label = prediction[0]
