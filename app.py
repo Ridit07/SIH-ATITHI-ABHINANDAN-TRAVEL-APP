9f3ae93e1573d1a3839964d55527d464fe9181e4
Ridit07 <112847735+Ridit07@users.noreply.github.com>
2023-09-14 23:56:18 +0530

    app.py

--- app.py
+++ app.py
@@ -4,28 +4,25 @@ import pickle
 
 model = pickle.load(open('decision_tree_model.pkl', 'rb'))
 
-st.title('which distination will he choose')
+st.title('Which Destination Will They Choose')
 
-Traveller_Profile_Type = st.slider("Traveller Profile Type",0,3)
-Choice_Preference = st.slider("Choice/Preference",0,3)
-Mode_of_Travel = st.slider("Mode of Travel",0,3)
-Budget = st.slider("Budget",10000,1000000)
-
+Traveller_Profile_Type = st.slider("Traveller Profile Type", 0, 3)
+Choice_Preference = st.slider("Choice/Preference", 0, 3)
+Mode_of_Travel = st.slider("Mode of Travel", 0, 3)
+Budget = st.slider("Budget", 10000, 1000000)
 
 
 def predict():
-    float_features = [float(x) for x in [Traveller Profile Type, Choice/Preference, Mode of Travel, Budget]]
+    float_features = [float(Traveller_Profile_Type), float(Choice_Preference), float(Mode_of_Travel), float(Budget)]
     final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
     label = prediction[0]
-    
-    print(type(label))
-    print(label)
 
-
-    if(int(label)==1):
-        st.success('Hureyyyy!! The costomer will make a purchase '  + ' :thumbsup:')
+    if int(label) == 1:
+        st.success('Hurray!! The customer will make a purchase :thumbsup:')
     else:
-        st.success('Ohhh The costomer wont make a purchase '  + ' :thumbsup:')
+        st.success('Ohh, the customer won\'t make a purchase :thumbsdown:')
 
-trigger = st.button('Predict', on_click=predict)
+
+if st.button('Predict'):
+    predict()
